6472e3a2df7b103f1d62217ebb0992b5995319f8
Kabiruux <uxkabiru@gmail.com>
2022-12-16 02:23:24 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - check the code
+ *
+ * Return: zero.
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar('0' + a);
+	}
+
+	_putchar('\n');
+}
