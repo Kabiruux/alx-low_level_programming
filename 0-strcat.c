4e1274413ed7f6ac9c2da9ff5cc2c0d584953643
Kabiruux <uxkabiru@gmail.com>
2022-12-22 15:05:10 -0800

    Two strings join

--- 0-strcat.c
+++ 0-strcat.c
@@ -1,30 +1,28 @@
 #include "main.h"
 
 /**
- * _strcat - concatenates two strings
- * @dest: string to append to
- * @src: string to add
- *
- * Return: a pointer to the resulting string
+ * _strcat - concatennates two strings
+ * @dest: copy to
+ * @src: copy from
+ * Return: pointer to dest
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	int i;
+	int j;
 
 	i = 0;
-	j = 0;
-
 	while (dest[i] != '\0')
+	{
 		i++;
-
+	}
+	j = 0;
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
-		j++;
 		i++;
+		j++;
 	}
-
 	dest[i] = '\0';
-
 	return (dest);
 }
